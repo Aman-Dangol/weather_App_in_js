7d15394dcad277ae010f1288e2b4529b305abc1e
Aman Dangol <amandangol423@gmail.com>
2023-12-10 22:27:25 +0545

    added gps

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const apikey = "d5e05bee320e5c93180fb77383f84007";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const apiUrlLat =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&lat={lat}&lon={lon}&appid={API key}";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -9,34 +12,10 @@ async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apikey}`);
   if (response.status === 404 || response.status === 400) {
     document.querySelector(".error").style.display = "block";
-    document.querySelector(".weather").style.display="none";
+    document.querySelector(".weather").style.display = "none";
   } else {
     var data = await response.json();
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-    switch (data.weather[0].main) {
-      case "Clouds":
-        weatherIcon.src = "images/clouds.png";
-        break;
-      case "Clear":
-        weatherIcon.src = "images/clear.png";
-        break;
-      case "Rain":
-        weatherIcon.src = "images/rain.png";
-        break;
-      case "Drizzle":
-        weatherIcon.src = "images/drizzle.png";
-        break;
-      case "Mist":
-        weatherIcon.src = "images/mist.png";
-        break;
-    }
-    document.querySelector(".weather").style.display = "block";
-    document.querySelector(".error").style.display = "none";
+    weather(data);
   }
 }
 
@@ -44,8 +23,63 @@ searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
 
-searchBox.addEventListener("keydown",(e)=>{
-    if(e.code === "Enter"){
-        checkWeather(searchBox.value);
-    }
-})
+searchBox.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
+
+getLocation();
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, error);
+  } else {
+    console.log("error");
+  }
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  // console.log(lat, lon);
+  latLongApi(lat, lon);
+}
+
+function error(e) {
+  console.log(e.message);
+}
+
+async function latLongApi(lat, lon) {
+  let response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${apikey}`
+  );
+  let data = await response.json();
+  weather(data);
+}
+
+function weather(data) {
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      weatherIcon.src = "images/clouds.png";
+      break;
+    case "Clear":
+      weatherIcon.src = "images/clear.png";
+      break;
+    case "Rain":
+      weatherIcon.src = "images/rain.png";
+      break;
+    case "Drizzle":
+      weatherIcon.src = "images/drizzle.png";
+      break;
+    case "Mist":
+      weatherIcon.src = "images/mist.png";
+      break;
+  }
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".error").style.display = "none";
+}
